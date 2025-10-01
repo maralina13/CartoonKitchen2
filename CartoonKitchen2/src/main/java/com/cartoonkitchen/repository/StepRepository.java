42d1d338ca1e6c9f8081e0704ff0b59eafd423ba
maralina13 <112951167+maralina13@users.noreply.github.com>
2025-10-02 01:56:01 +0300

    Delete CartoonKitchen2 directory

--- CartoonKitchen2/src/main/java/com/cartoonkitchen/repository/StepRepository.java
+++
@@ -1,7 +0,0 @@
-package com.cartoonkitchen.repository;
-
-import com.cartoonkitchen.entity.Step;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StepRepository extends JpaRepository<Step, Long> {
-}
--- CartoonKitchen2/src/main/java/com/cartoonkitchen/service/UserService.java
+++
@@ -1,8 +0,0 @@
-package com.cartoonkitchen.service;
-
-import com.cartoonkitchen.entity.User;
-
-public interface UserService {
-    void saveUser(User user);
-    User findByUsername(String username);
-}
